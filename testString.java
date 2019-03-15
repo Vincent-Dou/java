0093d0b34e6b828050c316937eec644b8a70b30c
Vincent-Dou <44737368+Vincent-Dou@users.noreply.github.com>
2019-03-15 22:25:13 +0800

    字符串常用方法

---
+++ testString.java
@@ -0,0 +1,55 @@
+package bitdemo1;
+
+import javax.swing.plaf.basic.BasicBorders.SplitPaneBorder;
+
+public class testString{
+	public static void main(String[] args) {
+//		String str0 = "hello";
+//		String str1 = new String("hello");
+//		String str2 = "hello";
+//		System.out.println(str0 == str1);
+//		System.out.println(str0 == str2);
+//		String str3 = "llo";
+//		System.out.println(str0.length());
+//		System.err.println(str0.charAt(index));
+//		String str4 = "heo";
+//		String str5 = "el";
+//		System.out.println(str0.indexOf(str5,0));
+//		System.out.println(str0.indexOf(str5,2));
+//		System.out.println(str0.substring(2,5));
+//		String str6 = "hello,world,java";
+//		String[] str7 = str6.split(",");
+//		for(int i = 0; i < str7.length; i++) {
+//			System.out.println(str7[i]);
+//		}
+//		String str8 = "Hello";
+//		String str9 = "Hello";
+//		System.out.println(str0.equals(str8));
+//		System.out.println(str9.equals(str8));
+//		System.out.println(str9.hashCode());
+		//str0.intern();
+//		str1.intern();
+//		String str2 = "hello";
+//		System.out.println(str2 == str1);
+//		System.out.println(str2 == str1.intern());
+//		String str1 = "hello";
+//		char[] str2 = str1.toCharArray();
+//		for(int i = 0; i < str2.length; i++) {
+//			System.out.println(str2[i]);
+//		}
+//		String str = "hello";
+//		String str2 = "";
+//		String str1 = null;
+//		System.out.println(str.isEmpty());
+//		System.out.println(str2.isEmpty());
+//		System.out.println(str1.isEmpty());
+//		String str = "   hello    ";
+//		System.out.println(str);
+//		System.out.println(str.trim());
+		String str1 = "hElLo";
+		System.out.println(str1.toUpperCase());
+		System.out.println(str1.toLowerCase());
+		System.out.println(str1.concat("world"));
+		System.out.println(str1+6);
+	}
+}
